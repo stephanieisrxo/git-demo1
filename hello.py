19481b598f1445e4a465b91a658e2ec9d4d7e47f
stephanie.israel <mary.israel31@thehutgroup.com>
2022-10-03 12:27:08 +0100

    Added Mars Feature

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
+import sys
+
 def hello():
+	if sys.argv[1] == "Mars":
+		hellomars()
+	else: 
+		helloworld()
+
+
+def hellomars():
+	print("Hello,Mars")
+
+
+def helloworld():
         print("Hello, World")
 
 if __name__ == '__main__':
