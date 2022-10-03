5774919544f1289fe5be927c969d8f1620c90c7f
stephanie.israel <mary.israel31@thehutgroup.com>
2022-10-03 15:47:42 +0100

    Added Jupiter feature

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
+import sys
+
 def hello():
+	if sys.argv[1] == "Jupiter":
+		hellojupiter()
+	else:
+		helloworld()
+
+def hellojupiter():
+	print("Hello, Jupiter")
+
+def helloworld():
         print("Hello, World")
 
 if __name__ == '__main__':
